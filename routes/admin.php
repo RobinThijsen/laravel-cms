0eba4dca863c69cfc08d0afd2e4580de0588530a
Robin <robin.t@live.be>
2025-01-11 20:29:23 +0100

    update route + fix provider

--- routes/admin.php
+++ routes/admin.php
@@ -2,13 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::prefix(config('cms.route.prefix'))->group(function () {
     Route::get('login', \LaravelCMS\Livewire\Auth\Login::class)->name('cms.login');
     Route::get('reset-password', \LaravelCMS\Livewire\Auth\ResetPassword::class)->name('cms.reset-password');
 
-    Route::prefix(config('cms.route.prefix'))
-        ->middleware(\LaravelCMS\Http\Middleware\AdminMiddleware::class)->group(function () {
+    Route::prefix('')->middleware(\LaravelCMS\Http\Middleware\AdminMiddleware::class)->group(function () {
         Route::get('', \LaravelCMS\Livewire\Admin\Dashboard::class)->name('cms.dashboard');
 
         Route::prefix('pages')->group(function () {
